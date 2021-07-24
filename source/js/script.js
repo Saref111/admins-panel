0d86b998bc5976932cb24172b063559473ce1b5d
Saref111 <saref012@gmail.com>
2021-07-24 15:47:31 +0300

    added jquery

---
+++ source/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('[data-picker="datepicker"]').datetimepicker({
+        timepicker:false,
+        format:'d-m-Y',
+        mask: true
+      });
+    $('[data-picker="timepicker"]').datetimepicker({
+        datepicker:false,
+        format:'H:m',
+        mask: true
+      });
+})
