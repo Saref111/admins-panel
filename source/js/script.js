7c184a16bd65b661fc19afcdbb22ab1fb8208ae4
saref012 <saref012@gmail.com>
2021-07-26 11:11:53 +0300

    added admins popup

--- source/js/script.js
+++ source/js/script.js
@@ -10,4 +10,14 @@ $(document).ready(() => {
         format:'H:m',
         mask: true
     });
+
+    $('[name="admins"]').change(function (e){
+        $(this).closest('.user-list__tr').toggleClass('user-list__tr--selected')
+    })
+
+    $('#all-checked').change(function (e) {
+        $('[name="admins"]').each(function (i, element) {
+            $(element).click();
+        })
+    })
 })
