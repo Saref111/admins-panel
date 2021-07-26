ce965579e44f2de3080af3b3f1d63fd711bde1bb
saref012 <saref012@gmail.com>
2021-07-26 13:07:39 +0300

    added reg popup

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,20 @@
+const isRegistration = () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    return Boolean(params.registration)
+}
+
+const showRegistrationPopup = () => {
+    if (isRegistration()) {
+
+        $('.popup--admin-registration').addClass('popup--open')
+    }
+}
+
+
+
+
 $(document).ready(() => {
     $('[data-picker="datepicker"]').datetimepicker({
         timepicker:false,
@@ -28,4 +45,37 @@ $(document).ready(() => {
     $('.popup__close').click(function(e){
         $(this).closest('.popup').removeClass('popup--open')
     })
+
+    $('.popup__textarea').on('input', function (e) {
+        const isValidInput = e.target.value !== ''
+
+        if (isValidInput) {
+            $(e.target).closest('form').find('.popup__submit').prop('disabled', false)
+        } else {
+            $(e.target).closest('form').find('.popup__submit').prop('disabled', true)
+        }
+    })
+
+    $('.popup__input--photo').on('input', function (e) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            $(this).closest('.popup__label').find('img').prop('src', reader.result)
+        }
+
+        reader.readAsDataURL(e.target.files[0])
+    })
+
+    $('.popup__show-pass').click(function () {
+        $(this).toggleClass('popup__show-pass--shown')
+        const isPass = $(this).closest('.popup__label').find('input').prop('type') === 'password'
+
+        if (isPass) {
+            $(this).closest('.popup__label').find('input').prop('type', 'text')
+        } else {
+            $(this).closest('.popup__label').find('input').prop('type', 'password')
+        }
+    })
+
+    showRegistrationPopup()
+
 })
