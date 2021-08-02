b703316076480d1becf8a1fb99360d5d0a8274df
saref012 <saref012@gmail.com>
2021-08-02 12:00:19 +0300

    added asc and desc sorting

--- source/js/vue-list.js
+++ source/js/vue-list.js
@@ -2,11 +2,12 @@ const ROUTES = {
   ADMINS: "https://dsscommunity.staj.fun/api/admin/admins/lists",
   USERS: "https://dsscommunity.staj.fun/api/admin/users/lists",
 };
-const getRoute = () => {
+const getRoute = (query) => {
   const pageName = document.querySelector("MAIN").id;
-  console.log(pageName);
 
-  return ROUTES[pageName.toUpperCase()];
+  return query
+    ? ROUTES[pageName.toUpperCase()] + "?" + query
+    : ROUTES[pageName.toUpperCase()];
 };
 
 const list = new Vue({
@@ -20,15 +21,10 @@ const list = new Vue({
     data: [],
     rawData: {},
     checked: 0,
+    orderByLogin: "asc",
   },
   async beforeMount() {
-    console.log(getRoute());
-    const res = await $.ajax(getRoute(), {
-      headers: {
-        Accept: "application/json; charset=utf-8",
-        "Content-Type": "application/json; charset=utf-8",
-      },
-    });
+    const res = await this.sendRequest();
 
     this.rawData = res.data;
     this.data = res.data.data;
@@ -43,15 +39,12 @@ const list = new Vue({
   },
   methods: {
     checkUnit(e) {
-      if (e.target.checked) {
-        this.checked++;
-      } else {
-        this.checked--;
-      }
-
-      if (this.checked < 0) {
-        this.checked = 0;
-      }
+      this.checked = 0;
+      document.querySelectorAll("[name='admins']").forEach((it) => {
+        if (it.checked) {
+          this.checked++;
+        }
+      });
     },
     setAllCheckboxes() {
       this.checked = 0;
@@ -61,6 +54,38 @@ const list = new Vue({
     },
     moment(args) {
       return moment(args);
+    },
+    async search() {
+      if (this.query.length > 2) {
+        const query = this.formQuery();
+        const resp = await this.sendRequest(query);
+        this.rawData = resp.data;
+        this.data = resp.data.data;
+      }
+    },
+    async sendRequest(query) {
+      const resp = await $.ajax(getRoute(query), {
+        headers: {
+          Accept: "application/json; charset=utf-8",
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      });
+      return { ...resp };
+    },
+    formQuery() {
+      let query = "user_login_order=" + this.orderByLogin;
+      if (this.query) {
+        query = query + "&search=" + this.query;
+      }
+      return query;
+    },
+    setOrderByLogin(e) {
+      this.orderByLogin = e.target.value;
+      const query = this.formQuery();
+      this.sendRequest(query);
+    },
+    clearQuery() {
+      this.query = "";
     },
   },
 
